060abcae820c576cc0c5f6d820e73c2e485b3db9
custs21113 <se_ccj@163.com>
2022-11-28 03:35:43 +0800

    save

--- src/service/config.js
+++ src/service/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = `http://localhost:3001` || "http://localhost:3000";
-export const LOCAL_URL =  `http://localhost:3003`||"http://localhost:3003";
+export const BASE_URL = `http://47.96.178.131:3001` || "http://localhost:3000";
+export const LOCAL_URL =  `http://47.96.178.131:3003`||"http://localhost:3003";
 export const TIME_OUT = 16000;
