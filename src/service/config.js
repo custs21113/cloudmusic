aa08dce9b77b9954d3de44b633e04eb558f926d1
custs21113 <se_ccj@163.com>
2022-11-28 18:36:10 +0800

    save

--- src/service/config.js
+++ src/service/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL =  process.env.MODE === 'production' ? `http://47.96.178.131:3001` : "http://localhost:3001";
+export const BASE_URL =  `http://47.96.178.131:3001`;
 export const LOCAL_URL =  `http://47.96.178.131:3003`||"http://localhost:3003";
 export const TIME_OUT = 16000;
